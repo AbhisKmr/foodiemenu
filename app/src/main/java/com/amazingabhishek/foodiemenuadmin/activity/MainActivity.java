f4c19393b7a04b661007e2046b431e7fd46f8706
Abhishek Kumar <akaks09@gmail.com>
2020-08-12 02:32:08 +0530

    firebase login added

---
+++ app/src/main/java/com/amazingabhishek/foodiemenuadmin/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.amazingabhishek.foodiemenuadmin.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.amazingabhishek.foodiemenuadmin.databinding.ActivityMainBinding;
+
+import javax.xml.transform.Result;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
