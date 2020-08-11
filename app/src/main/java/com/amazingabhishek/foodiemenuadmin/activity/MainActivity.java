eb4e4bf4d25eff24d240e47c13b80943ce190b4a
Abhishek Kumar <akaks09@gmail.com>
2020-08-12 03:49:11 +0530

    qr generater added

--- app/src/main/java/com/amazingabhishek/foodiemenuadmin/activity/MainActivity.java
+++ app/src/main/java/com/amazingabhishek/foodiemenuadmin/activity/MainActivity.java
@@ -1,22 +1,96 @@
 package com.amazingabhishek.foodiemenuadmin.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.amazingabhishek.foodiemenuadmin.R;
 import com.amazingabhishek.foodiemenuadmin.databinding.ActivityMainBinding;
+import com.amazingabhishek.foodiemenuadmin.databinding.NavHeaderMainBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
-import javax.xml.transform.Result;
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ActivityMainBinding binding;
+    private ActionBarDrawerToggle toggle;
+    private FirebaseAuth mAuth;
+    private ImageView qrImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
 
+        View headerView = binding.navView.inflateHeaderView(R.layout.nav_header_main);
+        qrImageView = headerView.findViewById(R.id.nav_header_imageView);
+
+        toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.navView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addItems:
+            Toast.makeText(this, "Item added", Toast.LENGTH_SHORT).show();
+            break;
+
+            case R.id.verifyCustomer:
+                try {
+                    qrImageView.setImageBitmap(encodeAsBitmap(mAuth.getCurrentUser().getPhoneNumber()));
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case R.id.logout:
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, PhoneAuthActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    private Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 250, 250, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 250, 0, 0, w, h);
+        return bitmap;
     }
 }
